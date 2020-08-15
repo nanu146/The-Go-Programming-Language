5599e10a00db01b50df3927abc1f84f1600bacbf
Bharadwaj Kopparthi <pradeep146nanu@gmail.com>
2020-08-15 16:39:36 +0530

    tempconv package update

---
+++ Chapter2/tempconv0/tempconv0.go
@@ -0,0 +1,22 @@
+package tempconv
+
+type Celsius float32
+type Fahrenheit float32
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	freezingC     Celsius = 0.0
+	boilingC      Celsius = 100.0
+)
+
+func main() {
+
+}
+
+func FtoC(f Fahrenheit) Celsius {
+	return Celsius(((f - 32) * 5) / 9)
+}
+
+func CtoF(c Celsius) Fahrenheit {
+	return Fahrenheit((c * 9 / 5) + 32)
+}
